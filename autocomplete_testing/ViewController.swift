dbf7a44bb2fc0b33a3fdfb0d3a6bbd43bca1ed86
Toni Jovanoski <minimoog77@gmail.com>
2019-03-13 23:53:53 +0100

    Initial XCode project

---
+++ autocomplete_testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autocomplete_testing
+//
+//  Created by Antonie Jovanoski on 3/13/19.
+//  Copyright © 2019 Antonie Jovanoski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
